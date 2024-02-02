35336e4340cb7d0708d8a44b22194f3c41c36b0f
cjjeon <hello@cjjeon.com>
2024-02-02 00:43:00 -0800

    adding authorization and window compatibility

--- src/app/ScrapeForm.tsx
+++ src/app/ScrapeForm.tsx
@@ -1,30 +1,57 @@
 'use client'
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 
 export default function ScrapeForm() {
-  const inputRef = useRef<HTMLInputElement>(null)
+    const [markdown, setMarkDown] = useState<string>('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const tokenRef = useRef<HTMLInputElement>(null)
 
-  const scrape = async () => {
-    if (!inputRef.current) return;
+    const scrape = async () => {
+        if (!inputRef.current) return;
+        const url = inputRef.current.value;
+        console.log(`url: ${url}`)
+        if (!url) return;
 
-    const res = await fetch('/api/scrape', {
-      method: 'POST',
-      body: JSON.stringify({
-        url: inputRef.current.value
-      })
-    })
+        if (!tokenRef.current) return;
+        const token = tokenRef.current.value;
+        console.log(`token: ${token}`)
+        if (!token) return;
 
-    const data = await res.json()
+        const res = await fetch('/api/scrape', {
+            method: 'POST',
+            body: JSON.stringify({
+                url: inputRef.current.value
+            }),
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        if (res.status !== 200) return;
+        const data = await res.json()
+        setMarkDown(data.markdown)
+    }
 
-    console.log(data)
-  }
+    return (
+        <div>
+            <div>
+                <div>URL</div>
+                <input ref={inputRef}/>
 
-  return (
-    <div>
-      <input ref={inputRef}/>
-      <button onClick={scrape}>
-        Scrape
-      </button>
-    </div>
-  );
+            </div>
+            <div>
+                <div>
+                    Token
+                </div>
+                <input ref={tokenRef}/>
+            </div>
+            <div>
+                <button onClick={scrape}>
+                    Scrape
+                </button>
+            </div>
+            <div>
+                {markdown}
+            </div>
+        </div>
+    );
 }
--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -3,76 +3,96 @@ import {NodeHtmlMarkdown} from "node-html-markdown";
 import puppeteer from "puppeteer-core";
 
 const sleep = (ms: number) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
 }
 
+async function getOption() {
+    if (process.env.NODE_ENV === 'production') {
+        return {
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+        }
+    }
 
-export async function POST(req: Request) {
-  console.log("environment mode: " + process.env.NODE_ENV)
+    if (process.platform === 'win32') {
+        return {
+            args: [],
+            executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+            headless: true
+        }
+    }
 
-  console.log("scraping...")
-
-  const {url, delay} = await req.json()
-  let timeDelay = delay || 4000
-
-  console.log("launching browser...")
-  const browser = await puppeteer.launch(
-    process.env.NODE_ENV === 'production'
-      ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-      : {
+    return {
         args: [],
         executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         headless: true
-      }
-  )
-
-  console.log("opening new page...")
-  const page = await browser.newPage()
-
-  console.log("setting request interception...")
-  await page.setRequestInterception(true)
-  page.on("request", (request) => {
-    const reqType = request.resourceType()
-    if (reqType === "document") {
-      request.continue()
-    } else if (process.env.NODE_ENV === "development") {
-      request.continue()
-    } else {
-      console.log("block request type: " + request.resourceType())
-      request.abort()
     }
-  })
+}
 
-  console.log("navigating to " + url + "...")
-  await page.goto(url, {timeout: 0})
+export async function POST(req: Request) {
+    const authorization = req.headers.get('Authorization')
+    if (!authorization) {
+        return new Response('Not Authorized', {status: 400})
+    }
+    const authToken = authorization.replace('Bearer ', '')
+    if (authToken !== process.env.AUTH_TOKEN) {
+        return new Response('Not Authorized', {status: 400})
+    }
 
-  if (process.env.NODE_ENV === "development") {
-    await sleep(4000)
-    console.log("add delay for javascript update")
-  }
+    console.log("environment mode: " + process.env.NODE_ENV)
+    console.log("scraping...")
 
-  console.log("get page content...")
+    const {url, delay} = await req.json()
+    let timeDelay = delay || 4000
 
-  const html =
-    process.env.NODE_ENV === "development"
-      ? await page.content()
-      : await page.evaluate(() => {
-        // @ts-ignore
-        return document.querySelector("body").innerHTML
-      })
+    console.log("launching browser...")
+    const option = await getOption()
+    const browser = await puppeteer.launch(option)
 
-  console.log("convert html to markdown...")
-  const markdown = NodeHtmlMarkdown.translate(html)
+    console.log("opening new page...")
+    const page = await browser.newPage()
 
-  console.log("closing browser...")
-  await browser.close()
+    console.log("setting request interception...")
+    await page.setRequestInterception(true)
+    page.on("request", (request) => {
+        const reqType = request.resourceType()
+        if (reqType === "document") {
+            request.continue()
+        } else if (process.env.NODE_ENV === "development") {
+            request.continue()
+        } else {
+            console.log("block request type: " + request.resourceType())
+            request.abort()
+        }
+    })
 
-  console.log("done.")
-  return Response.json({html, markdown})
+    console.log("navigating to " + url + "...")
+    await page.goto(url, {timeout: 0})
+
+    if (process.env.NODE_ENV === "development") {
+        await sleep(4000)
+        console.log("add delay for javascript update")
+    }
+
+    console.log("get page content...")
+
+    const html =
+        process.env.NODE_ENV === "development"
+            ? await page.content()
+            : await page.evaluate(() => {
+                // @ts-ignore
+                return document.querySelector("body").innerHTML
+            })
+
+    console.log("convert html to markdown...")
+    const markdown = NodeHtmlMarkdown.translate(html)
+
+    console.log("closing browser...")
+    await browser.close()
+
+    console.log("done.")
+    return Response.json({markdown})
 }
